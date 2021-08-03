8fe3e32afffeb01b31dec74509fac93f13a687b3
jungmini <mins0428@gmail.com>
2021-08-03 11:54:34 +0900

    NtosMini

--- Multi_PapagoTrans_De_En_Kr.Ready.php
+++ Multi_PapagoTrans_De_En_Kr.Ready.php
@@ -1,14 +1,20 @@
 <?php
 include_once(dirname(__FILE__).'/_common.php');
 
+
+if(!empty($pid)){
+	exec('taskkill /f /pid '.$pid);
+}
 exec('taskkill /im chrome.exe /f');
 //exec('taskkill /im cmd.exe /f');
 sleep(3);
 exec('start chrome');
 
 $sName = "_WinTrans";
+$TransName = "Multi_PapagoTrans_De_En_Kr";
+
 exec('schtasks /delete /tn "'.$sName.'" /f');
 
-passthru("/xampp/htdocs/_Ntos/_Trans/Multi_PapagoTrans_De_En_Kr.Ready.py");
+passthru("/xampp/htdocs/_Ntos/_Trans/".$TransName.".Ready.py");
 
-exec('c:/xampp/php/php.exe -f "c:/xampp/htdocs/_Ntos/_Trans/Multi_PapagoTrans_De_En_Kr.php"');
+exec('c:/xampp/php/php.exe -f "c:/xampp/htdocs/_Ntos/_Trans/'.$TransName.'.php"');
--- Multi_PapagoTrans_En_Kr.php
+++ Multi_PapagoTrans_En_Kr.php
@@ -4,111 +4,28 @@ include_once(dirname(__FILE__).'/_common.php');
 
 $Chk_Hi = (int)date("Hi");
 
-$RandTime = 120;	//초
 
-$SetStart = mktime(date('H'),date('i'),date('s')+$RandTime,date('m'),date('d'),date('Y'));
-$SetStartDay = date('Y/m/d', $SetStart);
-$SetStartTime = date('H:i', $SetStart);
+list($SetStartDay, $SetStartTime) = SetTime(120);
+
 
 $sName = "_WinTrans";
 
+$TransName = "Multi_PapagoTrans_En_Kr";
+
+$CustId = "amazon";
+
 $dir = 'C:/xampp/htdocs/_Ntos/_Trans';
-$TransData= CF_getDataURL('http://amazon.ntos.co.kr/_Mini_/_WinTrans/Multi_PapagoTrans_En_Kr.List.php?CustId=amazon&pc='.$pc);
 
 
+$TransData= CF_getDataURL('http://amazon.ntos.co.kr/_Mini_/_WinTrans/'.$TransName.'.List.php?CustId='.$CustId.'&pc='.$pc);
 
-switch($TransData){
-	case 'allstop' :
-	case 'stop' :
-		exec('taskkill /im chrome.exe /f');
-		
-		if($TransData == "stop"){
-			exec('schtasks /delete /tn "'.$sName.'" /f');
-			sleep(1);
-			exec('schtasks /create /tn "'. $sName .'" /tr c:\xampp\htdocs\_Ntos\_Trans\Multi_PapagoTrans_En_Kr.Ready.bat /sc once /st '. $SetStartTime .' /sd '. $SetStartDay);
-		} else {
-			exec('schtasks /delete /tn "'.$sName.'" /f');
-		}	//end if
-		exit;
-	break;
+//상단 공통
+include_once(dirname(__FILE__).'/_head.php');
 
-	case 'pull' :
-		exec('taskkill /im chrome.exe /f');
-
-		exec('schtasks /delete /tn "'.$sName.'" /f');
-		sleep(1);
-		exec('schtasks /create /tn "'. $sName .'" /tr c:\xampp\htdocs\_Ntos\_Trans\Multi_PapagoTrans_En_Kr.Ready.bat /sc once /st '. $SetStartTime .' /sd '. $SetStartDay);
-		if($pc != 1){
-			exec('c:\xampp\htdocs\_Ntos\_Trans\_GitPull.bat');
-		}	//end if
-		exit;
-	break;
-
-	case 'push' :
-		exec('taskkill /im chrome.exe /f');
-
-		exec('schtasks /delete /tn "'.$sName.'" /f');
-		sleep(1);
-		exec('schtasks /create /tn "'. $sName .'" /tr c:\xampp\htdocs\_Ntos\_Trans\Multi_PapagoTrans_En_Kr.Ready.bat /sc once /st '. $SetStartTime .' /sd '. $SetStartDay);
-		if($pc == 1){
-			exec('c:\xampp\htdocs\_Ntos\_Trans\_GitPush.bat');
-	}	//end if
-		exit;
-	break;
-
-	default :
-
-		$dest_file = $dir."/_Multi_PapagoTrans_En_Kr.txt";
-		$source_file = fopen($dest_file, "w") or die("Unable to open file!");
-		fwrite($source_file, $TransData);
-		fclose($source_file);
-
-		ob_start();
-		passthru("/xampp/htdocs/_Ntos/_Trans/Multi_PapagoTrans_En_Kr.py $pc ");
-		$Result = ob_get_clean(); 
-		if(preg_match('@success@', $Result)) {
-			$sType = "run";
-		} else {
-			$sType = "rerun";
-		}
-
-	break;
-
-}	//end switch
-
-unlink($dir."/_Multi_PapagoTrans_En_Kr.txt");
+//파일삭제
+unlink($dir."/_".$TransName.".txt");
 
 $sType = (empty($sType))?"run":$sType;
 
-if($pc == 1){
-	if($Chk_Hi >= "900" && $Chk_Hi <= "920"){
-		$SetStartTime = "09:21";
-		$sType = "ready";
-	} else if($Chk_Hi >= "1000" && $Chk_Hi <= "1020"){
-		$SetStartTime = "10:21";
-		$sType = "ready";
-	}
-}
-
-
-//스케줄 등록
-exec('schtasks /delete /tn "'.$sName.'" /f');
-sleep(1);
-exec('schtasks /create /tn "'.$sName.'" /tr c:\xampp\htdocs\_Ntos\_Trans\Multi_PapagoTrans_En_Kr.Ready.bat /sc once /st '.$SetStartTime.' /sd '.$SetStartDay);
-
-if($sType == "run"){	//바로시작
-	exec('c:\xampp\php\php.exe -f "c:\xampp\htdocs\_Ntos\_Trans\Multi_PapagoTrans_En_Kr.php"');
-	/*
-	exec('schtasks /delete /tn "'.$sName.'" /f');
-	exec('schtasks /create /tn "'.$sName.'" /tr c:\xampp\htdocs\_Ntos\_Trans\Multi_PapagoTrans_En_Kr.bat /sc once /st '.$SetStartTime.' /sd '.$SetStartDay);
-	exec('schtasks /run /tn "'.$sName.'" ');
-	*/
-} else if($sType == "rerun"){
-	sleep(1);
-	exec('taskkill /im chrome.exe /f');	
-	sleep(3);
-	exec('schtasks /run /tn "'.$sName.'" ');
-} else {
-	sleep(1);
-	exec('taskkill /im chrome.exe /f');	
-}	//end if
+//하단 공통
+include_once(dirname(__FILE__).'/_tail.php');
